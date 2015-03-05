b993e1377aed9790e3a5afa11bbc78af138a0bc2
Martin Abelson Sahlen <Sahlen@Martins-MacBook-Pro.local>
2015-03-05 21:41:54 +0100

    Add beginner server side impl

--- server/server.js
+++ server/server.js
@@ -7,27 +7,67 @@ var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-var router = express.Router();
-
-
-router.route('/')
-  .get(function(req, res){
-    res.send('Thisal asdmir oot');
-  });
-
-router.route('/users')
-  .get(function(req, res){
-    res.send('This will return a collection of all users')
-  });
-
-app.use('/api', router);
-
 app.use(express.static(__dirname + '/../client/build'));
 
+credentials = {
+	facebook: {
+		clientID: process.env.FACEBOOK_ID || '923195097723030',
+		clientSecret: process.env.FACEBOOK_SECRET || '41c17e0e110d358a738cffff24eef2f3',
+		callbackURL: '/auth/facebook/callback'
+	},
+	twitter: {
+		clientID: process.env.TWITTER_KEY || 'xP3wLS2YuUylvFAfTDtlfdRKJ',
+		clientSecret: process.env.TWITTER_SECRET || 'f4k88mZAWIJDc3EnCf98qMb6EZapsQmUO6NFrdiyGlrG6tegcP',
+		callbackURL: '/auth/twitter/callback'
+	},
+	twitterCounter: {
+		apiKey: '39578770216b332a668c8b7a5d3e218e'
+	},
+	instagram: {
+		clientID: process.env.INSTAGRAM_CLIENT_ID || '2ea4a18199f4443ca69c0be6e6180c86',
+		clientSecret: process.env.INSTAGRAM_CLIENT_SECRET || '417441686fac4bfaad89172d4ceb924a',
+		callbackURL: '/auth/instagram/callback'
+	},
+	soundcloud: {
+		clientID: process.env.SOUNDCLOUD_CLIENT_ID || '5791890dd6a8c62dfbe0294c26487095',
+		clientSecret: process.env.SOUNDCLOUD_CLIENT_SECRET || '3a94a6721fae03ac65ce6bc84ae2a214',
+		callbackURL: '/auth/soundcloud/callback'
+	},
+	spotify: {
+		clientID: '73f7b323b1894ccb86ff3584c7de2ce1',
+		clientSecret: 'd735c3da3ae14c2b88f7751cce8d5906',
+		callbackURL: '/auth/spotify/callback'
+	}
+};
 
-console.log('starting webserver on http://localhost:8080. Static root with API available from /api');
+// TWITTER SETUP
+TWITTER_CONSUMER_KEY = 'myUQ6sNZjLiZDc5tbzkLlBuCA';
+TWITTER_CONSUMER_SECRET = 'pdHti68cDuLhJp27h1TfZkAgVgUWdkjYoIW6PFk7OENAA8fg8G'
+TWITTER_ACCESS_TOKEN = '1511048018-ErgCI8AMrYpoF56p4grUSAFaZbmbpt1js7mEZ4c';
+TWITTER_ACCESS_SECRET = 'tUcevP3q4GMMewQw1HmnEhrqivjn8IC0Gc3YQ3ATzkZ5T';
 
-var port = process.env.PORT || 8080;
+var Twitter = require('twitter');
+var twitterClient = new Twitter({
+  consumer_key: TWITTER_CONSUMER_KEY,
+  consumer_secret: TWITTER_CONSUMER_SECRET,
+  access_token_key: TWITTER_ACCESS_TOKEN,
+  access_token_secret: TWITTER_ACCESS_SECRET,
+});
+// TWITTER SETUP
+
+// SOUNDCLOUD SETUP
+var request = require('request');
+request('https://api.soundcloud.com/tracks?client_id='+credentials.soundcloud.clientID, function(error, response, body){
+	
+});
+
+var apiRouter = express.Router();
+apiRouter.get('/', function(req, res){
+	res.send('hei');
+})
+
+app.use('/api', apiRouter);
+
+var port = 8080;
 
 app.listen(port);
