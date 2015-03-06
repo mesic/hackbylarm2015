bd04086825d3c4847727211d6d065746a8b07f8f
Nicolaj Broby <n@broby.co>
2015-03-06 11:34:44 +0100

    fixed fanbase json

--- client/src/components/about/about.js
+++ client/src/components/about/about.js
@@ -9,12 +9,18 @@ angular.module('mmApp.about', [])
 	aboutService.loadFanbase().query(function (data) {
 		$scope.fanbaseData = data;
 
-		
+		updateFanbase(data[0]);
+
 		
     });
 
+	function updateFanbase(data){
 
+		for(var k in data){
 
+			$('#'+k+'-total').html(data[k].total);
+			$('#'+k+'-sinceYesterday').html(data[k].sinceYesterday);
 
+	}
     
 }]);
